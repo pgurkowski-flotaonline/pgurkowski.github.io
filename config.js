fc91958c78fc80aff52d13abb607491c64834dc1
pgurkowski <p.gurkowski@flota-online.pl>
2017-12-20 10:34:52 +0100

    create

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAA58T1aks:APA91bG2_vribsgl9cZEhp5icmJoRhdFoRsKCGyKVn4Coyh3GdVKZrQytv24THvBhHyd2IuRnSSxCOSfiDovgAYA9R1fmZxFQ0j-RBTx7p6w0yB1NDQeCL-kT_nsxKLW5-uBr7hWAqmq'
+};
